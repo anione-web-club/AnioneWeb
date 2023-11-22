aeafed3804e45472ae6f8850b4d78b42b1abf8ba
현빈 <gusqls060303@naver.com>
2023-11-22 20:49:33 +0900

    프로젝트 페이지 반응형 수정

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,8 +4,8 @@ import styles from "@/styles/projects.module.css";
 
 export default function Project({ path, name }) {
   return (
-    <div>
-      <Link href={path}>
+    <div className={styles.flex}>
+      <Link href={path} className={styles.imgCover}>
         <Image src={`${path}.png`} width={426} height={240} alt={name} />
       </Link>
       <h3 className={styles.subtitle}>{name}</h3>
