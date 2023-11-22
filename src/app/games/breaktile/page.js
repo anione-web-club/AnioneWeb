27cb13767b3873d26ae51768a3cc05563373871d
현빈 <gusqls060303@naver.com>
2023-11-22 10:12:06 +0900

    웹 게임 페이지 기본 셋팅

--- src/app/games/breaktile/page.js
+++ src/app/games/breaktile/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useRef } from "react";
+import styles from "@/styles/games.module.css";
 
 export default function BreakTile() {
   const canvasRef = useRef(null);
@@ -8,107 +9,18 @@ export default function BreakTile() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    canvas.width = 420;
-    canvas.height = 420;
-
-    const playerSize = 10;
-    let x = canvas.width / 2;
-    let y = canvas.height / 2;
-    const moveSpeed = 2.5;
-
-    let upPressed = false;
-    let downPressed = false;
-    let leftPressed = false;
-    let rightPressed = false;
-
-    const brickLineCount = 12;
-    const brickSize = 30;
-    const brickOffset = 30;
-    const bricks = [];
-
-    for (let c = 0; c < brickLineCount; c++) {
-      bricks[c] = [];
-      for (let r = 0; r < brickLineCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
-      }
+    if (window.innerWidth <= 768) {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerWidth;
+    } else {
+      canvas.width = 420;
+      canvas.height = 420;
     }
-
-    function movePlayer() {
-      if (upPressed && y > playerSize) {
-        y -= moveSpeed;
-      } else if (downPressed && y < canvas.height - playerSize) {
-        y += moveSpeed;
-      } else if (rightPressed && x < canvas.width - playerSize) {
-        x += moveSpeed;
-      } else if (leftPressed && x > playerSize) {
-        x -= moveSpeed;
-      }
-    }
-
-    function drawPlayer() {
-      ctx.beginPath();
-      ctx.arc(x, y, playerSize, 0, Math.PI * 2);
-      ctx.fillStyle = "green";
-      ctx.fill();
-      ctx.closePath();
-    }
-
-    function drawBricks() {
-      for (let c = 0; c < brickLineCount; c++) {
-        for (let r = 0; r < brickLineCount; r++) {
-          const brickX = c * brickSize + brickOffset;
-          const brickY = r * brickSize + brickOffset;
-          bricks[c][r].x = brickX;
-          bricks[c][r].y = brickY;
-          ctx.beginPath();
-          ctx.strokeRect(brickX, brickY, brickSize, brickSize);
-          ctx.fillStyle = "#eee";
-          ctx.fill();
-          ctx.closePath();
-        }
-      }
-    }
-
-    function draw() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      movePlayer();
-
-      drawBricks();
-      drawPlayer();
-
-      requestAnimationFrame(draw);
-    }
-
-    function keyDownHandler(e) {
-      if (e.key === "Up" || e.key === "ArrowUp") {
-        upPressed = true;
-      } else if (e.key === "Down" || e.key === "ArrowDown") {
-        downPressed = true;
-      } else if (e.key === "Left" || e.key === "ArrowLeft") {
-        leftPressed = true;
-      } else if (e.key === "Right" || e.key === "ArrowRight") {
-        rightPressed = true;
-      }
-    }
-
-    function keyUpHandler(e) {
-      if (e.key === "Up" || e.key === "ArrowUp") {
-        upPressed = false;
-      } else if (e.key === "Down" || e.key === "ArrowDown") {
-        downPressed = false;
-      } else if (e.key === "Left" || e.key === "ArrowLeft") {
-        leftPressed = false;
-      } else if (e.key === "Right" || e.key === "ArrowRight") {
-        rightPressed = false;
-      }
-    }
-
-    document.addEventListener("keyup", keyUpHandler, false);
-    document.addEventListener("keydown", keyDownHandler, false);
-
-    draw();
   }, []);
 
-  return <canvas ref={canvasRef} />;
+  return (
+    <div>
+      <canvas ref={canvasRef} className={styles.alignCenter} />
+    </div>
+  );
 }
