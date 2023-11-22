cbd58268d95e1f420733eab3222c221c0e177986
Yurly123 <joo7won@gmail.com>
2023-11-22 20:27:06 +0900

    Add local-optimized fine-tuning model
    Yeah

--- src/app/anione-gpt/api/route.js
+++ src/app/anione-gpt/api/route.js
@@ -1,12 +1,24 @@
 import OpenAI from "openai"
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 const openai = new OpenAI()
+const system = readFileSync(join(
+    process.cwd(),
+    'public', 'anione-gpt', 'system.txt'
+)).toString()
 
 export async function POST(req) {
-    const messages = [await req.json()]
+    const messages = [
+        {
+            "role": "system",
+            "content": system
+        },
+        await req.json(),
+    ]
     const data = {
         messages,
-        model: 'gpt-3.5-turbo',
+        model: process.env.ANIONE_GPT_MODEL_ID,
         stream: false,
     }
     const response = await openai.chat.completions.create(data)
--- src/app/anione-gpt/page.js
+++ src/app/anione-gpt/page.js
@@ -11,7 +11,7 @@ export default function () {
             },
             body: JSON.stringify({
                 "role": "user",
-                "content": "안녕?"
+                "content": "넌 누구야"
             })
         }).then(res => res.json())
             .then(data => console.log(data))
