57abfb818f0ba2d5967c868b302bf67de7a3904e
Yurly123 <joo7won@gmail.com>
2023-11-24 23:04:51 +0900

    Include vercel's ai package
    It's useful for this project

--- src/app/anione-gpt/api/route.js
+++ src/app/anione-gpt/api/route.js
@@ -2,6 +2,7 @@ import OpenAI from "openai"
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { buildMessage } from "@/util/message";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 const openai = new OpenAI()
 const system = readFileSync(join(
@@ -9,16 +10,21 @@ const system = readFileSync(join(
     'public', 'anione-gpt', 'system.txt'
 )).toString()
 
+export const runtime = 'edge'
+
 export async function POST(req) {
+    const json = await req.json()
+    console.log(json)
     const messages = [
         buildMessage('system', system),
-        ...await req.json(),
+        ...json,
     ]
     const data = {
         messages,
         model: process.env.ANIONE_GPT_MODEL_ID,
-        stream: false,
+        stream: true,
     }
     const response = await openai.chat.completions.create(data)
-    return Response.json(response.choices[0].message)
+    const stream = OpenAIStream(response)
+    return new StreamingTextResponse(stream)
 }
--- src/app/anione-gpt/page.js
+++ src/app/anione-gpt/page.js
@@ -1,29 +1,22 @@
 "use client"
 
-import { buildMessage } from "@/util/message"
-import { useEffect, useState } from "react"
+import { useChat } from 'ai/react';
+import { useEffect } from 'react';
 
 export default function () {
-    const initalMessage = buildMessage('user', '넌 누구야?')
-    const [message, setMessage] = useState({})
+    const { messages, setMessages, reload } = useChat({
+        api: '/anione-gpt/api',
+    })
+    messages.forEach(message => console.log(message))
 
     useEffect(() => {
-        const messages = [initalMessage]
-        const body = JSON.stringify(messages)
-
-        fetch('anione-gpt/api', {
-            method: 'POST',
-            body,
-        }).then(res => res.json())
-            .then(data => {
-                setMessage(data)
-            })
+        setMessages([{
+            role: 'user',
+            content: '넌 누구냐?',
+        }])
     }, [])
     return (
         <>
-            <span>질문: {initalMessage.content}</span>
-            <br />
-            <span>응답: {message.content}</span>
         </>
     )
 }
