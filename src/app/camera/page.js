d9c549039cde84b918b7e34f9f6702298488b2d6
현빈 <gusqls060303@naver.com>
2023-11-28 12:11:28 +0900

    카메라로 사진 캡처 기능 추가

--- src/app/camera/page.js
+++ src/app/camera/page.js
@@ -1,8 +1,10 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const CameraPage = () => {
   const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [photoURL, setPhotoURL] = useState(null);
 
   useEffect(() => {
     const startCamera = async () => {
@@ -28,10 +30,36 @@ const CameraPage = () => {
     };
   }, []);
 
+  const capturePhoto = () => {
+    if (videoRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+
+      // Set canvas dimensions to match video stream
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+
+      // Capture video frame and draw it on the canvas
+      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+      // Convert canvas content to data URL and set as photoURL state
+      const dataURL = canvas.toDataURL("image/png");
+      setPhotoURL(dataURL);
+    }
+  };
+
   return (
     <div>
       <h1>Camera Page</h1>
       <video ref={videoRef} autoPlay playsInline />
+      <button onClick={capturePhoto}>Capture Photo</button>
+      {photoURL && (
+        <div>
+          <h2>Captured Photo</h2>
+          <img src={photoURL} alt="Captured" />
+        </div>
+      )}
+      <canvas ref={canvasRef} style={{ display: "none" }} />
     </div>
   );
 };
