9af192d7beb7b95829d7a3eae296717dfa7896ae
현빈 <gusqls060303@naver.com>
2023-11-28 09:48:09 +0900

    게임 종류별 랭킹 제작
    <select>로 게임을 선택하여 랭킹을 볼 수 있도록 제작

--- src/app/ranking/page.js
+++ src/app/ranking/page.js
@@ -1,13 +1,15 @@
 "use client";
+import { useInput } from "@/util/hooks";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "@/api/firebase";
 
 export default function () {
+  const [selectedGame, setSelectedGame] = useInput("DinoRush");
   const [rankings, setRankings] = useState([]);
 
   function getRankings() {
-    getDocs(collection(firestore, "DinoRush")).then((snapshot) => {
+    getDocs(collection(firestore, selectedGame)).then((snapshot) => {
       const rankings = snapshot.docs.map((doc) => doc.data());
       rankings.sort((a, b) => b.score - a.score);
       setRankings(rankings);
@@ -16,11 +18,17 @@ export default function () {
 
   useEffect(() => {
     getRankings();
-  }, []);
+  }, [selectedGame]);
 
   return (
     <div>
-      <h1>게임 랭킹 보드</h1>
+      <h1>{selectedGame} 랭킹 보드</h1>
+      <select onChange={setSelectedGame}>
+        <option value="DinoRush">DinoRush</option>
+        <option value="Speedy">Speedy</option>
+        <option value="WORRRD">WORRRD</option>
+        <option value="MeteorImpact">MeteorImpact</option>
+      </select>
       <ol>
         {rankings.map((player, index) => (
           <li key={index}>
