87cfe4bc1a4f4b720bc8cf2290009dfc9aa57a3e
현빈 <gusqls060303@naver.com>
2023-11-30 01:32:33 +0900

    자주 활용되는 기능 함수 제작

--- src/app/games/dormitory-room/page.js
+++ src/app/games/dormitory-room/page.js
@@ -11,122 +11,38 @@ export default function DormitoryRoom() {
 
     const imagePath = "/images/games/dormitory-room/";
 
-    if (window.innerWidth <= 768) {
+    if (window.innerWidth <= 1080) {
       canvas.width = window.innerWidth;
-      canvas.height = window.innerWidth;
+      canvas.height = canvas.width * 1.5;
     } else {
-      canvas.width = 420;
-      canvas.height = 420;
+      canvas.height = window.innerHeight - 75;
+      canvas.width = (canvas.height / 5) * 3;
     }
 
-    const imageWidth = (canvas.width / 4) * 3;
-    const imageHeight = (canvas.height / 4) * 3;
+    // 이동이 가능한지 확인하는 변수
+    let leftSideBarClick = false;
+    let rightSideBarClick = false;
+    let bottomBarClick = false;
 
-    const scenes = {
-      room: imagePath + "room.png",
-      bed: imagePath + "bed.png",
-      desk: imagePath + "desk.png",
-      washroom: imagePath + "washroom.jpg",
-      toilet: imagePath + "toilet.jpg",
-    };
+    const imageHeight = (canvas.width / 3) * 4;
 
-    function RoomScene() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const background = new Image();
 
-      const CameraScene = new Image();
-      CameraScene.src = scenes.room;
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-      CameraScene.onload = () => {
-        ctx.drawImage(CameraScene, 0, 0, canvas.width, imageHeight);
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, imageHeight);
 
-        LeftSideBar();
-        RightSideBar();
-      };
-
-      canvas.addEventListener("touchstart", TouchEvent);
-
-      function TouchEvent(e) {
-        const touchX = e.touches[0].clientX;
-        const touchY = e.touches[0].clientY;
-
-        if (touchX < canvas.width / 20 && touchY < imageHeight) ToiletScene();
-        else if (touchX > canvas.width - canvas.width / 20 && touchY < imageHeight) WashroomScene();
-      }
-    }
-
-    function ToiletScene() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      // 검정 배경
-      ctx.fillStyle = "#000";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-      const CameraScene = new Image();
-      CameraScene.src = scenes.toilet;
-
-      CameraScene.onload = () => {
-        const widthMargin = (canvas.width - imageWidth) / 2;
-        //ctx.drawImage(CameraScene, widthMargin, 0, imageWidth, canvas.height);
-
-        //BottomBar();
-
-        ctx.fillStyle = "rgb(255, 255, 255)";
-        ctx.fillRect(
-          canvas.width / 2 - canvas.width / 20,
-          canvas.height - canvas.height / 20,
-          canvas.width / 10,
-          canvas.height / 20
-        );
-      };
-
-      canvas.addEventListener("touchstart", TouchEvent);
-
-      function TouchEvent(e) {
-        const touchX = e.touches[0].clientX;
-        const touchY = e.touches[0].clientY;
-
-        // 하단 버튼 클릭 시 방으로 이동
-        if (
-          touchX > canvas.width / 2 - canvas.width / 40 &&
-          touchX < canvas.width / 2 + canvas.width / 40 &&
-          touchY > canvas.height - canvas.height / 20
-        )
-          RoomScene();
-      }
-    }
-
-    function WashroomScene() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      ctx.fillStyle = "#000";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-      const CameraScene = new Image();
-      CameraScene.src = scenes.washroom;
-
-      CameraScene.onload = () => {
-        const widthMargin = (canvas.width - imageWidth) / 2;
-        ctx.drawImage(CameraScene, widthMargin, 0, imageWidth, canvas.height);
-
-        BottomBar();
-      };
-
-      canvas.addEventListener("touchstart", TouchEvent);
-
-      function TouchEvent(e) {
-        const touchX = e.touches[0].clientX;
-        const touchY = e.touches[0].clientY;
-
-        // 하단 버튼 클릭 시 방으로 이동
-        if (
-          touchX > canvas.width / 2 - canvas.width / 20 &&
-          touchX < canvas.width / 2 + canvas.width / 20 &&
-          touchY > canvas.height - canvas.height / 20
-        )
-          RoomScene();
-      }
+    function init() {
+      leftSideBarClick = false;
+      rightSideBarClick = false;
+      bottomBarClick = false;
     }
 
     function LeftSideBar() {
+      leftSideBarClick = true;
+
       ctx.fillStyle = "rgb(50, 50, 50)";
       ctx.fillRect(0, 0, canvas.width / 20, imageHeight);
 
@@ -139,6 +55,8 @@ export default function DormitoryRoom() {
     }
 
     function RightSideBar() {
+      rightSideBarClick = true;
+
       ctx.fillStyle = "rgb(50, 50, 50)";
       ctx.fillRect(canvas.width - canvas.width / 20, 0, canvas.width / 20, imageHeight);
 
@@ -151,15 +69,118 @@ export default function DormitoryRoom() {
     }
 
     function BottomBar() {
+      bottomBarClick = true;
+
       ctx.fillStyle = "rgb(50, 50, 50)";
-      ctx.fillRect(0, canvas.height - canvas.height / 20, canvas.width, canvas.height / 20);
+      ctx.fillRect(0, imageHeight - canvas.height / 20, canvas.width, canvas.height / 20);
 
       ctx.fillStyle = "rgb(255, 255, 255)";
       ctx.beginPath();
-      ctx.moveTo(canvas.width / 2 - canvas.width / 40, canvas.height - canvas.height / 30);
-      ctx.lineTo(canvas.width / 2 + canvas.width / 40, canvas.height - canvas.height / 30);
-      ctx.lineTo(canvas.width / 2, canvas.height - canvas.height / 80);
+      ctx.moveTo(canvas.width / 2 - canvas.width / 40, imageHeight - canvas.height / 30);
+      ctx.lineTo(canvas.width / 2 + canvas.width / 40, imageHeight - canvas.height / 30);
+      ctx.lineTo(canvas.width / 2, imageHeight - canvas.height / 80);
       ctx.fill();
+    }
+
+    /**
+     * 이벤트를 발생시키는 함수
+     * @param {function} callback 이벤트 발생 후 실행할 함수
+     * @param {object} clickBox {x: 0, y: 0, width: 0, height: 0} 형태의 객체
+     * @param {object} clickPoint {x: 0, y: 0} 형태의 객체
+     */
+    function triggerEvent(callback, clickBox, clickPoint) {
+      const { x, y, width, height } = clickBox;
+      const { x: clickX, y: clickY } = clickPoint;
+
+      // 클릭 박스 윤곽선 그리기
+      // ctx.strokeStyle = "#f00";
+      // ctx.strokeRect(x, y, width, height);
+
+      // 클릭한 좌표가 박스 안에 있는지 확인
+      if (clickX > x && clickX < x + width && clickY > y && clickY < y + height) {
+        callback();
+      }
+    }
+
+    // 씬 구성
+    function RoomScene() {
+      background.src = imagePath + "정면_낮.png";
+      background.onload = () => {
+        init();
+
+        ctx.drawImage(background, 0, 0, canvas.width, imageHeight);
+
+        LeftSideBar();
+        RightSideBar();
+
+        console.log(leftSideBarClick, rightSideBarClick, bottomBarClick);
+      };
+
+      function RoomEvent(e) {
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        if (x < canvas.width / 20 && leftSideBarClick) ToiletScene();
+        if (x > canvas.width - canvas.width / 20 && rightSideBarClick) BathroomScene();
+
+        triggerEvent(
+          () => {
+            console.log("책상 클릭");
+          },
+          {
+            x: canvas.width / 2 - canvas.width / 10,
+            y: imageHeight / 2 - canvas.width / 10,
+            width: canvas.width / 5,
+            height: canvas.width / 5,
+          },
+          { x, y }
+        );
+      }
+
+      canvas.addEventListener("click", RoomEvent);
+    }
+
+    function ToiletScene() {
+      background.src = imagePath + "화장실_낮.png";
+      background.onload = () => {
+        init();
+
+        ctx.drawImage(background, 0, 0, canvas.width, imageHeight);
+
+        BottomBar();
+      };
+
+      function ToiletEvent(e) {
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        if (y > imageHeight - canvas.height / 20 && bottomBarClick) RoomScene();
+      }
+
+      canvas.addEventListener("click", ToiletEvent);
+    }
+
+    function BathroomScene() {
+      background.src = imagePath + "욕실_낮.png";
+      background.onload = () => {
+        init();
+
+        ctx.drawImage(background, 0, 0, canvas.width, imageHeight);
+
+        BottomBar();
+      };
+
+      function BathroomEvent(e) {
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        if (y > imageHeight - canvas.height / 20 && bottomBarClick) RoomScene();
+      }
+
+      canvas.addEventListener("click", BathroomEvent);
     }
 
     RoomScene();
