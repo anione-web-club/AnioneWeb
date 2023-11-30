54813b852dd9f12a37ce8074708243d842f1378e
현빈 <gusqls060303@naver.com>
2023-12-01 08:14:18 +0900

    애니원GPT 페이지 제작

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,29 +1,32 @@
-'use client'
+"use client";
 
-import { useChat } from 'ai/react';
+import { useChat } from "ai/react";
+import styles from "@/styles/chat.module.css";
 
 export function Chat() {
-    const { messages, input, handleInputChange, handleSubmit } = useChat({
-        api: '/anione-gpt/api',
-    });
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/anione-gpt/api",
+    initialMessages: [
+      { role: "user", content: "안녕하세요" },
+      { role: "assistant", content: "반갑습니다" },
+    ],
+  });
 
-    return (
-        <div>
-            <ul>
-                {messages.map((m, index) => (
-                    <li key={index}>
-                        {m.role === 'user' ? 'User: ' : 'AI: '}
-                        {m.content}
-                    </li>
-                ))}
-            </ul>
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input value={input} onChange={handleInputChange} placeholder="내용을 입력해주세요" />
 
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <input value={input} onChange={handleInputChange} />
-                </label>
-                <button type="submit">Send</button>
-            </form>
-        </div>
-    );
+        <button type="submit">Send</button>
+      </form>
+      <ul className={styles.messages}>
+        {messages.map((m, index) => (
+          <li key={index}>
+            {m.role === "user" ? "질문: " : "애니원GPT: "}
+            {m.content}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
